5a54de3ea2d2955ddb0024f4d7330e053d40c192
Jakored <98157755+jakored1@users.noreply.github.com>
2022-04-12 17:38:53 +0300

    Update for new version

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from distutils.core import setup
 setup(
   name = 'shaulmiko',         # How you named your package folder (shaulmiko)
   packages = ['shaulmiko'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Makes SSH sessions easier',   # Give a short description about your library
   author = 'Jakored and Ashal148148 on github',                   # Type in your name
   author_email = 'benja.bzness@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/jakored1/ShaulMiko',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jakored1/pypi-shaulmiko/archive/refs/tags/v0.3.tar.gz',    # I explain this later on
-  keywords = ['ShaulMiko', 'SSH', 'ssh', 'session', 'telnetlib', 'tty', 'cisco', 'router', 'switch', 'jakored', 'Ashal'],   # Keywords that define your package best
+  download_url = 'https://github.com/jakored1/pypi-shaulmiko/archive/refs/tags/v0.4.tar.gz',    # I explain this later on
+  keywords = ['ShaulMiko', 'SSH', 'ssh', 'session', 'telnetlib', 'tty', 'cisco', 'router', 'switch', 'jakored', 'Ashal', 'paramiko', 'netmiko'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'paramiko',
+          'typing',
       ],
   classifiers=[
-    'Development Status :: 5 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
